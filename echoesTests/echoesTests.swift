8c9ab54b1f6be079acda57011b60b053e28667fd
3instein <reynaldi_kindarto@yahoo.com>
2024-10-08 08:59:34 +0700

    Initial Commit

---
+++ echoesTests/echoesTests.swift
@@ -0,0 +1,17 @@
+//
+//  echoesTests.swift
+//  echoesTests
+//
+//  Created by Reynaldi Kindarto on 08/10/24.
+//
+
+import Testing
+@testable import echoes
+
+struct echoesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
