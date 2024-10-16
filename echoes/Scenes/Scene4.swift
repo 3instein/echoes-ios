5db179dce7ad916bc7c3c67f92cdf72051956990
Pelangi Masita <pelangimasita18@gmail.com>
2024-10-16 09:17:46 +0700

    chore: Remove all scenes

---
+++ echoes/Scenes/Scene4.swift
@@ -0,0 +1,24 @@
+//
+//  Scene4.swift
+//  echoes
+//
+//  Created by Pelangi Masita Wati on 15/10/24.
+//
+
+// GameScene.swift
+
+import SceneKit
+import UIKit
+
+class Scene4: SCNScene {
+
+    override init() {
+        super.init()
+        // Add any additional setup for the scene here
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        // Add any additional setup for the scene here
+    }
+}
