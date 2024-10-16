5951cd36225bc01c323253e2e8810a76179b5331
Elyora Dior <elyoradior@gmail.com>
2024-10-16 11:15:14 +0700

    Feed: Add opening scene video

---
+++ echoes/Scenes/SceneOpening.swift
@@ -0,0 +1,18 @@
+//
+//  SceneOpening.swift
+//  echoes
+//
+//  Created by Elyora Dior on 16/10/24.
+//
+
+import SwiftUI
+
+struct SceneOpening: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SceneOpening()
+}
