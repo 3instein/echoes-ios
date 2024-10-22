2a0c07fbe0a9b04aecac6421e14c0c548cab9016
Pelangi Masita <pelangimasita18@gmail.com>
2024-10-22 13:45:03 +0700

    scene2 echo

--- echoes/GameViewController.swift
+++ echoes/GameViewController.swift
@@ -20,14 +20,14 @@ class GameViewController: UIViewController {
         SceneManager.shared.configure(with: scnView)
 
         // Load the initial game scene
-        SceneManager.shared.loadScene4()
+        SceneManager.shared.loadScene2()
         
         // Set up joystick component
         joystickComponent = VirtualJoystickComponent()
         joystickComponent.attachToView(self.view)
 
         // Set up the PlayerEntity
-        if let gameScene = scnView.scene as? Scene4 {
+        if let gameScene = scnView.scene as? Scene2 {
             playerEntity = gameScene.playerEntity
             
             // Create a movement component to handle player movement, including the light node
@@ -49,7 +49,7 @@ class GameViewController: UIViewController {
         }
         
         //for Scene2
-        //playerEntity.movementComponent.movePlayer(to: SCNVector3(-15.538, -29.942, 0.728), duration: 20.0)
+        playerEntity.movementComponent.movePlayer(to: SCNVector3(-15.538, -29.942, 0.728), duration: 20.0)
 
         // Configure the SCNView
         scnView.allowsCameraControl = false
